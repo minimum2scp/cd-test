b6b7ffb6f472f17b6bdfefe351340117ebb19c33
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-11-04 19:48:12 +0900

    fix hubot-deploy.sh

--- hubot-deploy.sh
+++ hubot-deploy.sh
@@ -7,4 +7,8 @@ export -p
 pwd
 ls -la
 
+
+. /etc/profile.d/rbenv.sh
+mkdir -p vendor/bundle
+bundle install --path=vendor/bundle --jobs=4 --without development:test
 bundle exec cap ${DEPLOY_ENV} deploy BRANCH=${BRANCH}
