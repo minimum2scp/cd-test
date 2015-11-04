3da8857d7d82b297e792c7b512dcb8c7411aabd9
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-11-04 19:54:55 +0900

    test

--- hubot-deploy.sh
+++ hubot-deploy.sh
@@ -7,6 +7,8 @@ export -p
 pwd
 ls -la
 
+rbenv exec gem env
+
 mkdir -p vendor/bundle
 rbenv exec bundle install --path=vendor/bundle --jobs=4 --without development:test
 rbenv exec bundle exec cap ${DEPLOY_ENV} deploy BRANCH=${BRANCH}
